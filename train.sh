c7e980840b1b325d27a220e909284f855d13d3b2
Tzu-Ching Kuo <ben100719@gmail.com>
2023-06-20 22:24:08 +0800

    shell script

---
+++ train.sh
@@ -0,0 +1,8 @@
+python train.py\
+--model "efficientnet_b2"\
+--epoch 200\
+--num-classes 2\
+--data "/home/nas/Research_Group/Personal/Andrew/modelTraining/train_and_val"\
+--batch-size 64\
+--device 2\
+--imgsz 224
