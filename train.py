f75e4abf986b7d8382f713e8c1e77297731348d0
Tzu-Ching Kuo <ben100719@gmail.com>
2023-06-21 10:04:23 +0800

    ...

--- train.py
+++ train.py
@@ -97,7 +97,7 @@ def main(opt):
 
     """Train and evaluate"""    
     model_ft, val_acc_hist, val_loss_hist, train_acc_hist, train_loss_hist = train_model(model_ft, dataloaders_dict, criterion, optimizer_ft, device, num_epochs=num_epochs, is_inception=(model_name=="inception"), patience=patience)
-    torch.save(model_ft, os.path.join(PATH, '/run/weight.pth'))
+    torch.save(model_ft, 'weight.pth')
 
     plot_val_train_hist(num_epochs, val_loss_hist, train_loss_hist, model_name, 'Loss')
     plot_val_train_hist(num_epochs, val_acc_hist, train_acc_hist, model_name, 'Accuracy')
