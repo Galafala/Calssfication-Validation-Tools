3253c3f61ab633bfe362de4e068eb3a8b8bea31c
Tzu-Ching Kuo <ben100719@gmail.com>
2023-06-21 11:26:33 +0800

    ...

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 python train.py\
  --model "efficientnet_b2"\
- --epoch 1\
+ --epoch 200\
  --num-classes 2\
  --data "/home/nas/Research_Group/Personal/Andrew/birth_event_detection/dataset/train_and_val"\
  --batch-size 64\
