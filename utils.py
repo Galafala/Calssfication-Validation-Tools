93a82ef5aaa943796b21bf3a1ec81961e74e5cc0
Tzu-Ching Kuo <ben100719@gmail.com>
2023-06-21 10:50:37 +0800

    ...

--- utils.py
+++ utils.py
@@ -234,9 +234,9 @@ class ImageFolderWithPaths(datasets.ImageFolder):
         return tuple_with_path
 
 def plot_val_train_hist(num_epochs, val_hist, train_hist, model_name, Loss_or_Accuracy = 'Loss'):
-    val_hist = np.array([val for val in val_hist])
-    train_hist = np.array([train for train in train_hist])
-   
+    val_hist = torch.Tensor.cpu(val_hist)
+    train_hist = torch.Tensor.cpu(train_hist)
+    
     x=np.arange(0,num_epochs,1)
     plt.figure(figsize=(9,9))
     plt.plot(x, val_hist, label='test', color = 'limegreen', linewidth=2)
